47910148de456c6695c590c3f360c7177ba7fa6c
devrahul-2508 <devrahul2508@gmail.com>
2024-01-18 10:20:57 +0530

    migrated all listeners to bg service file

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 
+import 'package:altor_assignment/services/sensor_services.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_background_service/flutter_background_service.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:sensors_plus/sensors_plus.dart';
 
@@ -15,17 +17,17 @@ class _HomeScreenState extends State<HomeScreen> {
   // List to store accelerometer data
   List<AccelerometerEvent> _accelerometerValues = [];
 
-  // StreamSubscription for accelerometer events
-  late StreamSubscription<AccelerometerEvent> _accelerometerSubscription;
+  // // StreamSubscription for accelerometer events
+  // late StreamSubscription<AccelerometerEvent> _accelerometerSubscription;
 
   List<GyroscopeEvent> _gyroscopeValues = [];
 
-  // StreamSubscription for accelerometer events
+  // // StreamSubscription for accelerometer events
 
-  late StreamSubscription<GyroscopeEvent> _gyroscopeSubscription;
+  // late StreamSubscription<GyroscopeEvent> _gyroscopeSubscription;
 
   List<MagnetometerEvent> _magnometerValues = [];
-  late StreamSubscription<MagnetometerEvent> _magnetometerSubscription;
+  // late StreamSubscription<MagnetometerEvent> _magnetometerSubscription;
 
   late StreamSubscription<Position> _positionStreamSubscription;
   Position? _position;
@@ -34,42 +36,46 @@ class _HomeScreenState extends State<HomeScreen> {
   void initState() {
     // TODO: implement initState
     super.initState();
-
+    startSensorStream();
     _checkLocationPermission();
-    _accelerometerSubscription = accelerometerEventStream().listen((event) {
-      setState(() {
-        // Update the _accelerometerValues list with the latest event
-        _accelerometerValues = [event];
-      });
-    });
+    // _accelerometerSubscription = accelerometerEventStream().listen((event) {
+    //   setState(() {
+    //     // Update the _accelerometerValues list with the latest event
+    //     _accelerometerValues = [event];
+    //   });
+    // });
 
-    _gyroscopeSubscription = gyroscopeEventStream().listen((event) {
-      setState(() {
-        _gyroscopeValues = [event];
-      });
-    });
+    // _gyroscopeSubscription = gyroscopeEventStream().listen((event) {
+    //   setState(() {
+    //     _gyroscopeValues = [event];
+    //   });
+    // });
 
-    _magnetometerSubscription = magnetometerEventStream().listen((event) {
-      setState(() {
-        _magnometerValues = [event];
-      });
-    });
+    // _magnetometerSubscription = magnetometerEventStream().listen((event) {
+    //   setState(() {
+    //     _magnometerValues = [event];
+    //   });
+    // });
   }
 
   @override
   void dispose() {
     // TODO: implement dispose
     super.dispose();
-    _accelerometerSubscription.cancel();
-    _gyroscopeSubscription.cancel();
-    _magnetometerSubscription.cancel();
+    // _accelerometerSubscription.cancel();
+    // _gyroscopeSubscription.cancel();
+    // _magnetometerSubscription.cancel();
+
+    //disposeSensorStream();
   }
 
   void _startLocationFetchingStream() async {
     if (await Geolocator.isLocationServiceEnabled()) {
       _positionStreamSubscription =
           Geolocator.getPositionStream().listen((Position? position) {
-        _position = position;
+        setState(() {
+          _position = position;
+        });
       });
     } else {
       await Geolocator.openLocationSettings();
@@ -96,50 +102,100 @@ class _HomeScreenState extends State<HomeScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            SizedBox(height: 10),
+            StreamBuilder<Map<String, dynamic>?>(
+              stream: FlutterBackgroundService().on('update'),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+
+                final data = snapshot.data!;
+                String? device = data["device"];
+                DateTime? date = DateTime.tryParse(data["current_date"]);
+                return Column(
+                  children: [
+                    Text(device ?? 'Unknown'),
+                    Text(date.toString()),
+                  ],
+                );
+              },
+            ),
+            SizedBox(height: 10),
             Text(
               'Accelerometer Data:',
               style: TextStyle(fontSize: 20),
             ),
             SizedBox(height: 10),
-            if (_accelerometerValues.isNotEmpty)
-              Text(
-                'X: ${_accelerometerValues[0].x.toStringAsFixed(2)}, '
-                'Y: ${_accelerometerValues[0].y.toStringAsFixed(2)}, '
-                'Z: ${_accelerometerValues[0].z.toStringAsFixed(2)}',
-                style: TextStyle(fontSize: 16),
-              )
-            else
-              Text('No data available', style: TextStyle(fontSize: 16)),
+            StreamBuilder<Map<String, dynamic>?>(
+                stream:
+                    FlutterBackgroundService().on("updateAccelerometerData"),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    final data = snapshot.data!;
+                    var x = data['data']['x'];
+                    var y = data['data']['y'];
+                    var z = data['data']['z'];
+
+                    return Text(
+                      'X: ${x.toStringAsFixed(2)}, Y: ${y.toStringAsFixed(2)}, Z: ${z.toStringAsFixed(2)}',
+                      style: TextStyle(fontSize: 16),
+                    );
+                  } else
+                    return Text('Loading.....', style: TextStyle(fontSize: 16));
+                }),
+
             SizedBox(height: 10),
             Text(
-              'Gyrosocopic Data:',
+              'Gyroscope Data:',
               style: TextStyle(fontSize: 20),
             ),
-            SizedBox(height: 10),
-            if (_gyroscopeValues.isNotEmpty)
-              Text(
-                'X: ${_gyroscopeValues[0].x.toStringAsFixed(2)}, '
-                'Y: ${_gyroscopeValues[0].y.toStringAsFixed(2)}, '
-                'Z: ${_gyroscopeValues[0].z.toStringAsFixed(2)}',
-                style: TextStyle(fontSize: 16),
-              )
-            else
-              Text('No data available', style: TextStyle(fontSize: 16)),
+
+            StreamBuilder<Map<String, dynamic>?>(
+                stream:
+                    FlutterBackgroundService().on("updateGyroscopeData"),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    final data = snapshot.data!;
+                    var x = data['data']['x'];
+                    var y = data['data']['y'];
+                    var z = data['data']['z'];
+
+                    return Text(
+                      'X: ${x.toStringAsFixed(2)}, Y: ${y.toStringAsFixed(2)}, Z: ${z.toStringAsFixed(2)}',
+                      style: TextStyle(fontSize: 16),
+                    );
+                  } else
+                    return Text('Loading.....', style: TextStyle(fontSize: 16));
+                }),
+
             SizedBox(height: 10),
             Text(
               'Magnometer Data:',
               style: TextStyle(fontSize: 20),
             ),
             SizedBox(height: 10),
-            if (_magnometerValues.isNotEmpty)
-              Text(
-                'X: ${_magnometerValues[0].x.toStringAsFixed(2)}, '
-                'Y: ${_magnometerValues[0].y.toStringAsFixed(2)}, '
-                'Z: ${_magnometerValues[0].z.toStringAsFixed(2)}',
-                style: TextStyle(fontSize: 16),
-              )
-            else
-              Text('No data available', style: TextStyle(fontSize: 16)),
+            StreamBuilder<Map<String, dynamic>?>(
+                stream:
+                    FlutterBackgroundService().on("updateMagnetometerData"),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    final data = snapshot.data!;
+                    var x = data['data']['x'];
+                    var y = data['data']['y'];
+                    var z = data['data']['z'];
+
+                    return Text(
+                      'X: ${x.toStringAsFixed(2)}, Y: ${y.toStringAsFixed(2)}, Z: ${z.toStringAsFixed(2)}',
+                      style: TextStyle(fontSize: 16),
+                    );
+                  } else
+                    return Text('Loading.....', style: TextStyle(fontSize: 16));
+                }),
+            ///////
+
             SizedBox(height: 10),
             if (_position != null)
               Text(
--- lib/services/background_services.dart
+++ lib/services/background_services.dart
@@ -1,7 +1,9 @@
 import 'dart:async';
 import 'dart:ui';
 
+import 'package:altor_assignment/services/sensor_services.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_background_service/flutter_background_service.dart';
 import 'package:flutter_background_service_android/flutter_background_service_android.dart';
 import 'package:sensors_plus/sensors_plus.dart';
@@ -18,18 +20,68 @@ Future<void> initializeService() async {
           onStart: onStart, isForegroundMode: true, autoStart: true));
 }
 
+const MethodChannel backgroundChannel = MethodChannel('background_channel');
+
 @pragma('vm:entry-point')
 void onStart(ServiceInstance service) async {
   // List to store accelerometer data
-  List<AccelerometerEvent> _accelerometerValues = [];
+  // startSensorStream();
 
-  // StreamSubscription for accelerometer events
+  List<AccelerometerEvent> _accelerometerValues = [];
   late StreamSubscription<AccelerometerEvent> _accelerometerSubscription;
 
   _accelerometerSubscription = accelerometerEventStream().listen((event) {
     // Update the _accelerometerValues list with the latest event
     _accelerometerValues = [event];
+
+    Map<String, double> eventData = {
+      'x': event.x.toDouble(),
+      'y': event.y.toDouble(),
+      'z': event.z.toDouble(),
+    };
+
+    service.invoke(
+      'updateAccelerometerData',
+      {"data": eventData},
+    );
   });
+
+  List<GyroscopeEvent> _gyroscopeValues = [];
+  late StreamSubscription<GyroscopeEvent> _gyroscopeSubscription;
+
+  _gyroscopeSubscription = gyroscopeEventStream().listen((event) {
+    _gyroscopeValues = [event];
+
+    Map<String, double> eventData = {
+      'x': event.x.toDouble(),
+      'y': event.y.toDouble(),
+      'z': event.z.toDouble(),
+    };
+
+    service.invoke(
+      'updateGyroscopeData',
+      {"data": eventData},
+    );
+  });
+
+  List<MagnetometerEvent> _magnometerValues = [];
+  late StreamSubscription<MagnetometerEvent> _magnetometerSubscription;
+
+  _magnetometerSubscription = magnetometerEventStream().listen((event) {
+    _magnometerValues = [event];
+
+    Map<String, double> eventData = {
+      'x': event.x.toDouble(),
+      'y': event.y.toDouble(),
+      'z': event.z.toDouble(),
+    };
+
+    service.invoke(
+      'updateMagnetometerData',
+      {"data": eventData},
+    );
+  });
+
   DartPluginRegistrant.ensureInitialized();
   if (service is AndroidServiceInstance) {
     service.on('setAsForeground').listen((event) {
@@ -58,7 +110,13 @@ void onStart(ServiceInstance service) async {
 
     print("Background service running");
 
-    service.invoke('update');
+    service.invoke(
+      'update',
+      {
+        "current_date": DateTime.now().toIso8601String(),
+        "device": "Samsung",
+      },
+    );
   });
 }
 
